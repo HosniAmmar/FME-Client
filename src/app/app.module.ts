a08fe87c2e62eca6d97706c0c9d258fcb668e3d8
HosniAmmar <hosniammar123@gmail.com>
2018-11-09 19:49:25 +0100

    Add-Player  Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddPlayerComponent } from './Components/add-player/add-player.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddPlayerComponent
   ],
   imports: [
     BrowserModule,
