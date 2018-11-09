085dbd1b77e733fbe7173e4c9d9412a805289be5
HosniAmmar <hosniammar123@gmail.com>
2018-11-09 19:19:43 +0100

    Player Model

---
+++ src/app/Models/Player.ts
@@ -0,0 +1,56 @@
+export class Player {
+  id: number;
+  licenseNumber: string;
+  firstName: string;
+  lastName: string;
+  squadNumber: number;
+  natIdNum: string;
+  address: string;
+  phone: string;
+  mailAddress: string;
+  height: number;
+  weight: number;
+  birthday: string;
+  actualTeamId: number;
+  previousTeamId: number;
+  nationality: string;
+  category: string;
+  photoUrl: string;
+
+
+  constructor(id: number,
+              licenseNumber: string,
+              firstName: string,
+              lastName: string,
+              squadNumber: number,
+              natIdNum: string,
+              address: string,
+              phone: string,
+              mailAddress: string,
+              height: number,
+              weight: number,
+              birthday: string,
+              actualTeamId: number,
+              previousTeamId: number,
+              nationality: string,
+              category: string,
+              photoUrl: string) {
+    this.id = id;
+    this.licenseNumber = licenseNumber;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.squadNumber = squadNumber;
+    this.natIdNum = natIdNum;
+    this.address = address;
+    this.phone = phone;
+    this.mailAddress = mailAddress;
+    this.height = height;
+    this.weight = weight;
+    this.birthday = birthday;
+    this.actualTeamId = actualTeamId;
+    this.previousTeamId = previousTeamId;
+    this.nationality = nationality;
+    this.category = category;
+    this.photoUrl = photoUrl;
+  }
+}
